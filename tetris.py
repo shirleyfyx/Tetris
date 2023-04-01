a057ccf8eefbea9bcced72bc44575c8b5ea72a49
Shirley Fang <fangyixuan0123@gmail.com>
2023-03-31 20:02:48 -0400

    Finished the scoring feature and displayed the score in the game.

--- tetris.py
+++ tetris.py
@@ -19,7 +19,7 @@ class TEXT:
                             text='score', fgcolor='orange',
                             size=TILE_SIZE * 1.4, bgcolor='black')
         self.font.render_to(self.app.screen, (WIN_W * 0.64, WIN_H * 0.8),
-                            text='000', fgcolor='white',
+                            text=f'{self.app.tetris.score}', fgcolor='white',
                             size=TILE_SIZE * 1.8)
 
 class Tetris:
@@ -30,6 +30,14 @@ class Tetris:
         self.tetromino = Tetromino(self)
         self.next_tetronimo = Tetromino(self, current=False)
         self.speed_up = False
+
+        self.score = 0
+        self.full_lines = 0
+        self.points_per_lines = {0: 0, 1: 100, 2: 300, 3: 700, 4: 1500}
+
+    def get_score(self):
+        self.score += self.points_per_lines[self.full_lines]
+        self.full_lines = 0
 
     def check_full_lines(self):
     # Delete any full lines in the blocks.
@@ -47,6 +55,8 @@ class Tetris:
                 for x in range(FIELD_W):
                     self.field_array[row][x].alive = False
                     self.field_array[row][x] = 0
+
+                self.full_lines += 1
 
     def put_tetromino_blocks_in_array(self):
         for block in self.tetromino.blocks:
@@ -103,6 +113,7 @@ class Tetris:
             self.check_tetromino_landing()
             self.tetromino.update()
             self.check_full_lines()
+            self.get_score()
         self.sprite_group.update()
 
     def draw(self):
